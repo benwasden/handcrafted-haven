20319948a6650a85f6fd36ce9b4dc4175f4dbf41
benwasden <was24001@byui.edu>
2025-12-09 18:11:35 -0800

    Fixes to auth

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,18 @@
-export { auth as default } from "@/auth";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+
+export default NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      // You can leave this blank if proxy does not need authorize()
+      authorize() {
+        return null;
+      },
+    }),
+  ],
+}).auth;
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
